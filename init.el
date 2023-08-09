96e1d45ae9d72327d0dfeb33de0c6beab12164a7
Lawrence Logoh <lawrence.logoh@gmail.com>
2023-08-09 14:34:47 +0000

    Cleaned up emacs config
    I'm also switching back to ansi-term.
    term and shell don't work the way I expect

--- init.el
+++ init.el
@@ -1,14 +1,1 @@
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(markdown-mode windresize htmlize magit emmet-mode eglot reformatter company pydoc python-mode go-mode modus-themes use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
