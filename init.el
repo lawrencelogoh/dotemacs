14a3c602cd4a686296d5643162ec71f356ed98d7
Lawrence Logoh <lawrence.logoh@gmail.com>
2025-10-06 04:01:51 +0000

    Add yasnippet

--- init.el
+++ init.el
@@ -54,6 +54,55 @@
 (use-package nix-ts-mode)
 (use-package flymake-eslint)
 (use-package auctex)
+(use-package jtsx
+  :ensure t
+  :mode (("\\.jsx?\\'" . jtsx-jsx-mode)
+         ("\\.tsx\\'" . jtsx-tsx-mode)
+         ("\\.ts\\'" . jtsx-typescript-mode))
+  :commands jtsx-install-treesit-language
+  :hook ((jtsx-jsx-mode . hs-minor-mode)
+         (jtsx-tsx-mode . hs-minor-mode)
+         (jtsx-typescript-mode . hs-minor-mode))
+  :custom
+  ;; Optional customizations
+  (js-indent-level 2)
+  (typescript-ts-mode-indent-offset 2)
+  ;; (jtsx-switch-indent-offset 0)
+  ;; (jtsx-indent-statement-block-regarding-standalone-parent nil)
+  ;; (jtsx-jsx-element-move-allow-step-out t)
+  ;; (jtsx-enable-jsx-electric-closing-element t)
+  ;; (jtsx-enable-electric-open-newline-between-jsx-element-tags t)
+  ;; (jtsx-enable-jsx-element-tags-auto-sync nil)
+  (jtsx-enable-all-syntax-highlighting-features t)
+  :config
+  (defun jtsx-bind-keys-to-mode-map (mode-map)
+    "Bind keys to MODE-MAP."
+    (define-key mode-map (kbd "C-c C-j") 'jtsx-jump-jsx-element-tag-dwim)
+    (define-key mode-map (kbd "C-c j o") 'jtsx-jump-jsx-opening-tag)
+    (define-key mode-map (kbd "C-c j c") 'jtsx-jump-jsx-closing-tag)
+    (define-key mode-map (kbd "C-c j r") 'jtsx-rename-jsx-element)
+    (define-key mode-map (kbd "C-c <down>") 'jtsx-move-jsx-element-tag-forward)
+    (define-key mode-map (kbd "C-c <up>") 'jtsx-move-jsx-element-tag-backward)
+    (define-key mode-map (kbd "C-c C-<down>") 'jtsx-move-jsx-element-forward)
+    (define-key mode-map (kbd "C-c C-<up>") 'jtsx-move-jsx-element-backward)
+    (define-key mode-map (kbd "C-c C-S-<down>") 'jtsx-move-jsx-element-step-in-forward)
+    (define-key mode-map (kbd "C-c C-S-<up>") 'jtsx-move-jsx-element-step-in-backward)
+    (define-key mode-map (kbd "C-c j w") 'jtsx-wrap-in-jsx-element)
+    (define-key mode-map (kbd "C-c j u") 'jtsx-unwrap-jsx)
+    (define-key mode-map (kbd "C-c j d n") 'jtsx-delete-jsx-node)
+    (define-key mode-map (kbd "C-c j d a") 'jtsx-delete-jsx-attribute)
+    (define-key mode-map (kbd "C-c j t") 'jtsx-toggle-jsx-attributes-orientation)
+    (define-key mode-map (kbd "C-c j h") 'jtsx-rearrange-jsx-attributes-horizontally)
+    (define-key mode-map (kbd "C-c j v") 'jtsx-rearrange-jsx-attributes-vertically))
+    
+  (defun jtsx-bind-keys-to-jtsx-jsx-mode-map ()
+      (jtsx-bind-keys-to-mode-map jtsx-jsx-mode-map))
+
+  (defun jtsx-bind-keys-to-jtsx-tsx-mode-map ()
+      (jtsx-bind-keys-to-mode-map jtsx-tsx-mode-map))
+
+  (add-hook 'jtsx-jsx-mode-hook 'jtsx-bind-keys-to-jtsx-jsx-mode-map)
+  (add-hook 'jtsx-tsx-mode-hook 'jtsx-bind-keys-to-jtsx-tsx-mode-map))
 
 ;; Speed up startup
 (setq gc-cons-threshold most-positive-fixnum
@@ -264,6 +313,13 @@
 (setq org-habit-graph-column 45)
 (setq org-habit-show-habits-only-for-today nil)
 
+;; Yasnippet
+(setq yas-snippet-dirs
+      '("~/.config/emacs/snippets"            
+        ))
+
+(yas-global-mode 1)
+
 (setq org-startup-indented t)
 (setq org-indent-mode-turns-on-hiding-stars nil)
 ;; spellcheck
@@ -431,8 +487,8 @@
   (unless (treesit-ready-p (car source))
     (treesit-install-language-grammar (car source))))
 
-(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
+;; (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
+;; (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
 (add-to-list 'major-mode-remap-alist '(python-mode . python-ts-mode))
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
 (add-to-list 'auto-mode-alist '("\\.hs\\'" . haskell-ts-mode))
@@ -442,6 +498,7 @@
 
 ;; Wakatime
 (global-wakatime-mode)
+(setq wakatime-api-key (getenv "WAKATIME_API_KEY"))
 ;; Make eglot faster with tsserver
 (fset #'jsonrpc--log-event #'ignore)
 ;; corfu
@@ -515,6 +572,12 @@
 (add-hook 'rjsx-mode-hook  'emmet-mode)
 
 (setq ledger-reconcile-default-commodity "GHS")
+
+(setq ledger-reports
+      '(("Balance"              "ledger -f %(ledger-file) bal")
+        ("Debts Overview"       "ledger -f %(ledger-file) bal Assets:Receivables Liabilities:Payables")
+        ("Receivables register" "ledger -f %(ledger-file) reg Assets:Receivables")
+        ("Payables register"    "ledger -f %(ledger-file) reg Liabilities:Payables")))
 
 (defun config-reload ()
   (interactive)
